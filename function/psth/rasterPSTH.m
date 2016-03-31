ceb4e28eeaa574114fdd1a1461f35040034f8c97
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-03-31 23:42:08 +0900

    rasterPSTH
    calculate PSTH

---
+++ function/psth/rasterPSTH.m
@@ -0,0 +1,66 @@
+function [xpt,ypt,spikeBin,spikeHist,spikeConv,spikeConvZ] = rasterPSTH(spikeTime, trialIndex, win, binSize, resolution, dot)
+%rasterPSTH converts spike time into raster plot
+%   spikeTime: cell array. each cell contains vector array of spike times per each trial. unit is msec
+%   trialIndex: number of rows should be same as number of trials (length of spikeTime)
+%   win: window range of xpt. should be 2 numbers. unit is msec.
+%   binsize: unit is msec.
+%   resolution: sigma for convolution = binsize * resolution.
+%   dot: 1-dot, 0-line
+%   unit of xpt will be msec.
+narginchk(5, 6);
+if isempty(spikeTime) || isempty(trialIndex) || length(spikeTime) ~= size(trialIndex,1) || length(win) ~= 2
+    xpt = []; ypt = []; spikeBin = []; spikeHist = []; spikeConv = []; spikeConvZ = [];
+    return;
+end;
+
+spikeBin = win(1):binSize:win(2); % unit: msec
+nSpikeBin = length(spikeBin);
+
+nTrial = length(spikeTime);
+nCue = size(trialIndex,2);
+trialResult = sum(trialIndex);
+resultSum = [0 cumsum(trialResult)];
+
+yTemp = [0:nTrial-1; 1:nTrial; NaN(1,nTrial)]; % template for ypt
+xpt = cell(1,nCue);
+ypt = cell(1,nCue);
+spikeHist = zeros(nCue,nSpikeBin);
+spikeConv = zeros(nCue,nSpikeBin);
+
+for iCue = 1:nCue
+    if trialResult(iCue) == 0
+        spikeHist(iCue,:) = NaN;
+        spikeConv(iCue,:) = NaN;
+        continue; 
+    end
+    
+    % raster
+    nSpikePerTrial = cellfun(@length,spikeTime(trialIndex(:,iCue)));
+    nSpikeTotal = sum(nSpikePerTrial);
+    if nSpikeTotal == 0; continue; end;
+    
+    spikeTemp = cell2mat(spikeTime(trialIndex(:,iCue)))';
+    
+    xptTemp = [spikeTemp;spikeTemp;NaN(1,nSpikeTotal)];
+    if (nargin == 6) && (dot==1)
+        xpt{iCue} = xptTemp(2,:);
+    else
+        xpt{iCue} = xptTemp(:);
+    end
+
+    yptTemp = [];
+    for iy = 1:trialResult(iCue)
+        yptTemp = [yptTemp repmat(yTemp(:,resultSum(iCue)+iy),1,nSpikePerTrial(iy))];
+    end
+    if (nargin == 6) && (dot==1)
+        ypt{iCue} = yptTemp(2,:);
+    else
+        ypt{iCue} = yptTemp(:);
+    end
+
+    % psth
+    spkhist_temp = histc(spikeTemp,spikeBin)/(binSize/10^3*trialResult(iCue));
+    spkconv_temp = conv(spkhist_temp,fspecial('Gaussian',[1 5*resolution],resolution),'same');
+    spikeHist(iCue,:) = spkhist_temp;
+    spikeConv(iCue,:) = spkconv_temp;
+end

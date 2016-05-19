4983e0580da8ad0127c691a772a3bf9c669567b9
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-05-19 19:47:31 +0900

    MWM retrieval calc

--- MWM-project/MWM_retrieval.m
+++ MWM-project/MWM_retrieval.m
@@ -1,108 +1,83 @@
-xlsfile = FindFiles('*.xls');
-nFile = length(xlsfile);
+clearvars;
 
 %% Data load
-for iFile = 1:nFile
-    [filePath, fileName, ext] = fileparts(xlsfile{iFile});
+load Retrieval_Rbp4.mat
+Rbp4.probeQuadCNO = probeQuadCNO;
+Rbp4.probeQuadDMSO = probeQuadDMSO;
+
+load Retrieval_Grik4.mat
+Grik4.probeQuadCNO = probeQuadCNO;
+Grik4.probeQuadDMSO = probeQuadDMSO;
+
+load Retrieval_Camk2a.mat
+Camk2a.probeQuadCNO = probeQuadCNO;
+Camk2a.probeQuadDMSO = probeQuadDMSO;
+
+clear probeQuadCNO probeQuadDMSO
+
+%%
+nCol = 3;
+nRow = 2;
+wideInterval = [0.07, 0.07];
+
+%%
+
+% barYvalue = [Rbp4.probeQuadDMSO.NE(1),Rbp4.probeQuadCNO.NE(1);Rbp4.probeQuadDMSO.SE(1),Rbp4.probeQuadCNO.SE(1);...
+%     Rbp4.probeQuadDMSO.SW(1),Rbp4.probeQuadCNO.SW(1);Rbp4.probeQuadDMSO.NW(1),Rbp4.probeQuadCNO.NW(1)];
+% barError = [Rbp4.probeQuadDMSO.NE(2),Rbp4.probeQuadCNO.NE(2);Rbp4.probeQuadDMSO.SE(2),Rbp4.probeQuadCNO.SE(2);...
+%     Rbp4.probeQuadDMSO.SW(2),Rbp4.probeQuadCNO.SW(2);Rbp4.probeQuadDMSO.NW(2),Rbp4.probeQuadCNO.NW(2)];
+
+barXvalue = [];
+
+barYvalue.Rbp4 = [Rbp4.probeQuadDMSO.NE(1), Rbp4.probeQuadDMSO.SE(1), Rbp4.probeQuadDMSO.SW(1), Rbp4.probeQuadDMSO.NW(1);...
+    Rbp4.probeQuadCNO.NE(1),Rbp4.probeQuadCNO.SE(1),Rbp4.probeQuadCNO.SW(1),Rbp4.probeQuadCNO.NW(1)];
+
+barEvalue.Rbp4 = [Rbp4.probeQuadDMSO.NE(2), Rbp4.probeQuadDMSO.SE(2), Rbp4.probeQuadDMSO.SW(2), Rbp4.probeQuadDMSO.NW(2);...
+    Rbp4.probeQuadCNO.NE(2),Rbp4.probeQuadCNO.SE(2),Rbp4.probeQuadCNO.SW(2),Rbp4.probeQuadCNO.NW(2)];
+
+barYvalue.Grik4 = [Grik4.probeQuadDMSO.NE(1), Grik4.probeQuadDMSO.SE(1), Grik4.probeQuadDMSO.SW(1), Grik4.probeQuadDMSO.NW(1);...
+    Grik4.probeQuadCNO.NE(1),Grik4.probeQuadCNO.SE(1),Grik4.probeQuadCNO.SW(1),Grik4.probeQuadCNO.NW(1)];
+
+barEvalue.Grik4 = [Grik4.probeQuadDMSO.NE(2), Grik4.probeQuadDMSO.SE(2), Grik4.probeQuadDMSO.SW(2), Grik4.probeQuadDMSO.NW(2);...
+    Grik4.probeQuadCNO.NE(2),Grik4.probeQuadCNO.SE(2),Grik4.probeQuadCNO.SW(2),Grik4.probeQuadCNO.NW(2)];
+
+barYvalue.Camk2a = [Camk2a.probeQuadDMSO.NE(1), Camk2a.probeQuadDMSO.SE(1), Camk2a.probeQuadDMSO.SW(1), Camk2a.probeQuadDMSO.NW(1);...
+    Camk2a.probeQuadCNO.NE(1),Camk2a.probeQuadCNO.SE(1),Camk2a.probeQuadCNO.SW(1),Camk2a.probeQuadCNO.NW(1)];
+
+barEvalue.Camk2a = [Camk2a.probeQuadDMSO.NE(2), Camk2a.probeQuadDMSO.SE(2), Camk2a.probeQuadDMSO.SW(2), Camk2a.probeQuadDMSO.NW(2);...
+    Camk2a.probeQuadCNO.NE(2),Camk2a.probeQuadCNO.SE(2),Camk2a.probeQuadCNO.SW(2),Camk2a.probeQuadCNO.NW(2)];
+
+width = 0.95;
+barXpt = 1:4;
+colorDMSO = [1,0,1];
+hBar(1) = axes('Position',axpt(nCol,nRow,1,1,[],wideInterval));
+    hold on
+    for iStrata = 1:4
+        h.barRbp4_DMSO(iStrata) = bar(barXpt(iStrata),barYvalue.Rbp4(1,iStrata),width,'FaceColor',colorDMSO);
+        h.errorbar_DMSO(iStrata) = errorbar(barXpt(iStrata),barYvalue.Rbp4(1,iStrata),barEvalue.Rbp4(2,iStrata));
+    hold on
+    end
     
-    disp(['### Analyzing ', xlsfile{iFile}, '...']);
-    cd(filePath);
     
-    all_info = readtable([fileName, ext]);
+%     [b,e] = errorbarbar(barXvalue,barYvalue.Rbp4,barEvalue.Rbp4);
+    ylabel('Time (%)');
+    title('DG');
     
-    quad.NE(:,iFile) = all_info.TimeInZone____QuadrantNE;
-    quad.SE(:,iFile) = all_info.TimeInZone____QuadrantSE;
-    quad.SW(:,iFile) = all_info.TimeInZone____QuadrantSW;
-    quad.NW(:,iFile) = all_info.TimeInZone____QuadrantNW;
-end
-
-nTrial = size(quad.NE,1);
-probeTrial = nTrial-3;
-
-probeQuad.NE = quad.NE(probeTrial,:);
-probeQuad.SE = quad.SE(probeTrial,:);
-probeQuad.SW = quad.SW(probeTrial,:);
-probeQuad.NW = quad.NW(probeTrial,:);
-fields = fieldnames(probeQuad);
-
-%% Group discrimination
-%%%%%%%%%%%%%%%%%% Rbp4 %%%%%%%%%%%%%%%%%%%%%%
-% 1:1-1                 10:3-1
-% 2:1-2                 11:3-2
-% 3:1-3                 12:3-3
-% 4:1-4                 13:3-4 (No expression on L.hemi)
-% 5:2-1                 14:4-1 
-% 6:2-2                 15:4-2 (No expression on L.hemi)
-% 7:2-3 (No expression) 16:4-3
-% 8:2-4                 17:4-4
-% 9:2-5                 18:4-5
-
-
-%%%%%%%%%%%%%%%%%% Grik4 %%%%%%%%%%%%%%%%%%%%%%
-% 1:1-1(No expression on R.hemi)    9:3-1(No expresssion on R.hemi)
-% 2:1-2                            10:3-2
-% 3:1-3                            11:3-3
-% 4:1-4                            12:3-4
-% 5:2-1                            13:4-1
-% 6:2-2                            14:4-2
-% 7:2-3                            15:4-3
-% 8:2-4                            16:4-4 
-%                                  17:4-5
-
-
-%%%%%%%%%%%%%%%%%% Camk2a %%%%%%%%%%%%%%%%%%%%%%
-%CNO group:             DMSO group
-% 10                        1
-% 11                        2
-% 12                        3
-% 13                        4
-% 14                        5
-% 15                        6
-% 16                        7
-% 17                        8
-% 18                        9
-% 23: No expression         19
-% 24: No expression         20: No expression
-% 25                        21:
-% 26: No expression         22: Unilateral expression
-
-
-if regexp(fileName,'Rbp4');
-    idxCNO = [];
-    idxDMSO = [];
-elseif regexp(fileName,'Grik4');
-    idxCNO = [];
-    idxDMSO = [];
-else regexp(fileName,'Camk2a');
-    idxCNO = [10:18,25];
-    idxDMSO = [1:9,19,20,21,22];
-end
+hBar(2) = axes('Position',axpt(nCol,nRow,2,1,[],wideInterval));
+    hold on
+    h_barGrik4 = bar(barYvalue.Grik4);
+    hold on
+    title('Grik4');
+%     errorbarbar(barYvalue.Grik4,barEvalue.Grik4);
     
-for iField = 1:numel(fields)
-    probeQuadCNO.(fields{iField})(1) = mean(probeQuad.(fields{iField})(idxCNO));
-    probeQuadCNO.(fields{iField})(2) = std(probeQuad.(fields{iField})(idxCNO),1)/sqrt(length(idxCNO));
-    probeQuadDMSO.(fields{iField})(1) = mean(probeQuad.(fields{iField})(idxDMSO));
-    probeQuadDMSO.(fields{iField})(2) = std(probeQuad.(fields{iField})(idxCNO),1)/sqrt(length(idxDMSO));
-end
-
-barXvalue = [1,1.2; 2,2.2; 3,3.2; 4,4.2];
-barYvalue = [probeQuadDMSO.NE(1),probeQuadCNO.NE(1);probeQuadDMSO.SE(1),probeQuadCNO.SE(1);...
-    probeQuadDMSO.SW(1),probeQuadCNO.SW(1);probeQuadDMSO.NW(1),probeQuadCNO.NW(1)];
-barError = [probeQuadDMSO.NE(2),probeQuadCNO.NE(2);probeQuadDMSO.SE(2),probeQuadCNO.SE(2);...
-    probeQuadDMSO.SW(2),probeQuadCNO.SW(2);probeQuadDMSO.NW(2),probeQuadCNO.NW(2)];
-
-bar(barXvalue,barYvalue,4)
-hold on;
-errorbar(barXvalue(1,1),barYvalue(1,1),barError(1,1))
-% hold on;
-% errorbar(barValue,barError,'none')
-
-
-
-% barWidth = 1;
-% barGroupnames = {'NE';'SE';'SW';'NW'};
-errorbar_groups(barValue,barError)
-% ,barWidth,barGroupnames,[],[],[],'jet','none',[],2,'axis');
-
-
-
+hBar(3) = axes('Position',axpt(nCol,nRow,3,1,[],wideInterval));
+    hold on
+    h_barCamk2a = bar(barYvalue.Camk2a);
+    hold on
+    title('Camk2a');
+%     errorbarbar(barYvalue.Camk2a,barEvalue.Camk2a);
+    
+%     set(hBar,'YLim',[0,40],'XLim',[0.5,2.5]);
+    
+    
+    

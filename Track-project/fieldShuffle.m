26405aa248c92985c610181a08837b77dafbd453
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-05-25 17:49:42 +0900

    field Shuffle

--- Track-project/fieldShuffle.m
+++ Track-project/fieldShuffle.m
@@ -3,11 +3,15 @@ function fieldShuffle()
 alpha_v = 0.005;
 fr_threshold = 3;
 fieldsize_cutoff = 10;
-field_ratio = [72 48];
+field_ratio = [72, 48];
 
 
 %% Loading data
-[ttFile, nCell] = tfilecollector;
+% [ttFile, nCell] = tfilecollector;
+
+[tData, tList] = tLoad;
+nCell = length(tList);
+
 load ('VT1.mat');
 load('Events.mat','sensor','fields','nSensor','nTrial');
 
@@ -20,14 +24,13 @@ for iSeq = 1:1000
     randPosition{iSeq,1} = position(randSeq,:);
 end
 
-ttdata = LoadSpikes(ttFile,'tsflag','ts','verbose',0);
-
+% ttdata = LoadSpikes(ttFile,'tsflag','ts','verbose',0);
 for icell = 1:nCell
-    [cellPath,cellName,~] = fileparts(ttFile{icell});
-    disp(['### Analyzing ',ttFile{icell}, '...']);
+    [cellPath,cellName,~] = fileparts(tList{icell});
+    disp(['### Analyzing ',tList{icell}, '...']);
     cd(cellPath);
     
-    spkData = Data(ttdata{icell})/10; %unit: msec
+    spkData = tData{icell}; %unit: msec
     
     r.original = fieldPcorr(period1,period2,timestamp,position,spkData);
     
@@ -42,11 +45,3 @@ end
 
 disp('### Shuffling is done! ###');
 end
-
-    
-    
-    
-    
-
-    
-    

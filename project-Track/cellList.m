ac3bb6553f96a1c9890db94eada472bbd45e657c
Jun <jun.fruitmarket@gmail.com>
2016-06-09 10:08:13 +0900

    change

--- project-Track/cellList.m
+++ project-Track/cellList.m
@@ -14,17 +14,15 @@ T = table();
 for iFile = 1:nFile
     load(matFile{iFile},'fr_base','fr_task','burstIdx','spkwv','spkwth','hfvwth');
     fileSeg = strsplit(matFile{iFile},{'\','_'});
-%     valueSetLine = {'Rbp6'};
-%     valueSetType = {'DRun','DRw','nolight'};
     mouseLine = categorical(cellstr(fileSeg{5}));
     taskType = categorical(cellstr(fileSeg{9}));
     taskProb = categorical(cellstr(fileSeg{10}));
        
-    temT = table(mouseLine,taskType,{matFile(iFile)},taskProb,fr_base,fr_task,burstIdx,{spkwv},spkwth,hfvwth,...
-        'VariableNames',{'mouseLine','taskType','Path','taskProb','fr_base','fr_task','burstIdx','spkwv','spkwth','hfwth'});
+    temT = table(mouseLine,taskType,{matFile(iFile)},taskProb,fr_base,fr_task,burstIdx,{spkwv},spkwth,hfvwth,p_tag,p_Modu,...
+        'VariableNames',{'mouseLine','taskType','Path','taskProb','fr_base','fr_task','burstIdx','spkwv','spkwth','hfwth','p_tag','p_Modu'});
     
     T = [T; temT];
     
 end
 
-
+save('cellList.mat','T');

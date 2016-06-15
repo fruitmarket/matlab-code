79c44b8ed2abe1e84aab0047189f9b00d5c25eee
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-06-15 20:02:42 +0900

    fisher's Z-transformation

---
+++ function/stats/fisherZ.m
@@ -0,0 +1,14 @@
+function z = fisherZ(r)
+% Fisher's z-transformation
+%
+% r: matrix (ex. results from Pearson's correlation values)
+% z: Fisher's z-transformation value
+%
+% Author: Junyeop Lee
+% Version: 1.0 (6. 15. 2016)
+
+r = r(:);
+n = length(r);
+    z = 1/2.*log((1+r)./(1-r));
+    stdError = 1/(sqrt(n-3));
+end

a27fa16d1499c8caff059aed278396b52501e654
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-08-27 20:04:15 +0900

    tagStat change

--- project-Track/tagstatTrack.m
+++ project-Track/tagstatTrack.m
@@ -3,10 +3,11 @@ function tagstatTrack(sessionFolder)
 
 % Variables
 dt = 0.1;
-testRangeTag = 20; % unit: ms 
-baseRangeTag = 400; % baseline 
-testRangeModu = 20; % 8Hz: 20 // 20Hz: 20
-baseRangeModu = 80; % 8Hz: 100 // 20Hz: 15
+testRangeTag = 10; % unit: ms 
+baseRangeTag = 450; % baseline 
+testRangeModu = 10; % 8Hz: 10 // 20Hz: 20
+baseRangeModu = 110; % 8Hz: 110 // 20Hz: 15
+latency = 0; % 4ms laser latency
 
 % Find files
 if nargin == 0; sessionFolder = {}; end;
@@ -23,63 +24,64 @@ for iCell = 1:nCell
     load('Events.mat','lightTime');
     spikeData = tData{iCell};
     
-    [timeTag, censorTag] = tagDataLoad(spikeData, lightTime.Tag, testRangeTag, baseRangeTag);
-    [timeModu, censorModu] = tagDataLoad(spikeData, lightTime.Modu, testRangeModu, baseRangeModu);
+    [timeTag, censorTag] = tagDataLoad(spikeData, lightTime.Tag+latency, testRangeTag, baseRangeTag);
+    [timeModu, censorModu] = tagDataLoad(spikeData, lightTime.Modu+latency, testRangeModu, baseRangeModu);
     
-    [p_tag,time_tag,H1_tag,H2_tag] = logRankTest(timeTag, censorTag); % H1: light induced firing H2: baseline
-    save([cellName,'.mat'],...
-        'p_tag','time_tag','H1_tag','H2_tag','-append');
-      
-    [p_modu,time_modu,H1_modu,H2_modu] = logRankTest(timeModu, censorModu);
-    if isempty(p_modu)
-        p_modu = 1;
+    % Log-rank test
+    [pLR_tag,timeLR_tag,H1_tag,H2_tag] = logRankTest(timeTag, censorTag); % H1: light induced firing H2: baseline     
+    [pLR_modu,timeLR_modu,H1_modu,H2_modu] = logRankTest(timeModu, censorModu);
+    if isempty(pLR_modu)
+        pLR_modu = 1;
     end
     save([cellName,'.mat'],...
-        'p_modu','time_modu','H1_modu','H2_modu','-append');
+        'pLR_tag','timeLR_tag','H1_tag','H2_tag','-append',...
+        'pLR_modu','timeLR_modu','H1_modu','H2_modu','-append');
     
     % Salt test
-    [p_saltTag, l_saltTag] = saltTest(timeTag, testRangeTag, dt);
-    save([cellName,'.mat'],...
-        'p_saltTag','l_saltTag','-append');
-    
-    [p_saltModu, l_saltModu] = saltTest(timeModu, testRangeModu, dt);
-    if isempty(p_saltModu)
-        p_saltModu = 1;
+    [pSaltTag, lSaltTag] = saltTest(timeTag, testRangeTag, dt);
+    [pSaltModu, lSaltModu] = saltTest(timeModu, testRangeModu, dt);
+    if isempty(pSaltModu)
+        pSaltModu = 1;
     end
     save([cellName,'.mat'],...
-        'p_saltModu','l_saltModu','-append');
+        'pSaltTag','lSaltTag',...
+        'pSaltModu','lSaltModu','-append');
     
     % Modulation direction (activation/inactivation) & light latency
+    % Latency is calculated by the first point of 30% of H1_modu/tag(end)
     if ~isempty(nonzeros(H1_tag)) && ~isempty(nonzeros(H2_tag))
         if H1_tag(end) > H2_tag(end)
-            lightDir_tag = 1; % Activation
+            tagStatDir_tag = 1; % Activation
         else
-            lightDir_tag = 0; % Inactivation
+            tagStatDir_tag = -1; % Inactivation
         end
-        latency_tag = min(time_tag(find((H1_tag(end)/10<H1_tag),1,'first')));
+        latency_tag = min(timeLR_tag(find((H1_tag(end)*3/10<H1_tag),1,'first')));
     else
-        lightDir_tag = 2; % No modulation
+        tagStatDir_tag = 0; % No modulation
         latency_tag = NaN;
     end
     
     if isempty(lightTime.Modu);
-        lightDir_modu = 2; % No modulation
+        tagStatDir_modu = 2; % No modulation
         latency_modu = NaN;
     else
         if ~isempty(nonzeros(H1_modu)) && ~isempty(nonzeros(H2_modu))
             if H1_modu(end) > H2_modu(end)
-                lightDir_modu = 1;
+                tagStatDir_modu = 1;
             else
-                lightDir_modu = 0;
+                tagStatDir_modu = -1;
             end
-            latency_modu = min(time_modu(find((H1_modu(end)/10<H1_modu),1,'first')));
+            latency_modu = min(timeLR_modu(find((H1_modu(end)*3/10<H1_modu),1,'first')));
         else
-            lightDir_modu = 2;
+            tagStatDir_modu = 0;
             latency_modu = NaN;
         end
     end
+    latencyTag = latency + latency_tag;
+    latencyModu = latency + latency_modu;
+    
     save([cellName, '.mat'],...
-        'lightDir_tag','lightDir_modu','latency_tag','latency_modu','-append');
+        'tagStatDir_tag','tagStatDir_modu','latencyTag','latencyModu','-append');
 end
 disp('### Tag stat test done!');
 
@@ -103,7 +105,7 @@ if isempty(onsetTime); time = []; censor = []; return; end;
 nLight = length(onsetTime);
 
 % Rearrange data
-bin = [-floor(baseRange/testRange)*testRange:testRange:0]; % because of the latency (5ms), changed from 0 to 5.
+bin = [-floor(baseRange/testRange)*testRange:testRange:0];
 nBin = length(bin);
 
 binMat = ones(nLight,nBin)*diag(bin);

074f99d0e2144f8c4f452680bddfcfc908a55be4
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-09-12 20:29:54 +0900

    change
    peakMap added

--- project-Track/cellList.m
+++ project-Track/cellList.m
@@ -1,5 +1,4 @@
 clearvars;
-
 rtPath = 'D:\Dropbox\SNL\P2_Track';
 startingDir = {'D:\Projects\Track_151029-4_Rbp6';'D:\Projects\Track_151029-5_Rbp8';'D:\Projects\Track_151213-2_Rbp14';'D:\Projects\Track_160221-1_Rbp16'};
 
@@ -17,7 +16,7 @@ for iFile = 1:nFile
         'pLR_tag','pLR_modu','pSaltTag','pSaltModu','statDir_tag','statDir_modu','testLatencyModu_first','testLatencyModu_second',...                                         % From tagstatTrack
         'testLatencyTag_first','testLatencyTag_second','baseLatencyModu','baseLatencyTag','pLatencyModu_first','pLatencyModu_second','pLatencyTag_first','pLatencyTag_second',...   % From tagstatTrack
         'lightSpk','lightPreSpk','lightPostSpk','psdPreSpk','psdPostSpk','lighttagSpk','lighttagPreSpk','lighttagPostSpk','intraLightDir','interLightDir','tagLightDir',...         % From pethLight
-        'r_Corrbfxaft','r_Corrbfxdr','r_Corrdrxaft','r_Corrhfxhf','r_CorrEvOd','p_Corrbfxaft','p_Corrbfxdr','p_Corrdrxaft','p_Corrhfxhf','p_CorrEvOd');                             % mapCorr & EvOd
+        'peakMap','r_Corrbfxaft','r_Corrbfxdr','r_Corrdrxaft','r_Corrhfxhf','r_CorrEvOd','p_Corrbfxaft','p_Corrbfxdr','p_Corrdrxaft','p_Corrhfxhf','p_CorrEvOd');                             % mapCorr & EvOd
     fileSeg = strsplit(matFile{iFile},{'\','_'});
     mouseLine = categorical(cellstr(fileSeg{5}));
     taskType = categorical(cellstr(fileSeg{9}));
@@ -27,12 +26,12 @@ for iFile = 1:nFile
         pLR_tag,pLR_modu,pSaltTag,pSaltModu,statDir_tag,statDir_modu,testLatencyModu_first,testLatencyModu_second,...
         testLatencyTag_first,testLatencyTag_second,baseLatencyModu,baseLatencyTag,pLatencyModu_first,pLatencyModu_second,pLatencyTag_first,pLatencyTag_second,...
         lightSpk,lightPreSpk,lightPostSpk,psdPreSpk,psdPostSpk,lighttagSpk,lighttagPreSpk,lighttagPostSpk,intraLightDir,interLightDir,tagLightDir,...
-        r_Corrbfxaft,r_Corrbfxdr,r_Corrdrxaft,r_Corrhfxhf,r_CorrEvOd,p_Corrbfxaft,p_Corrbfxdr,p_Corrdrxaft,p_Corrhfxhf,p_CorrEvOd,...
+        peakMap,r_Corrbfxaft,r_Corrbfxdr,r_Corrdrxaft,r_Corrhfxhf,r_CorrEvOd,p_Corrbfxaft,p_Corrbfxdr,p_Corrdrxaft,p_Corrhfxhf,p_CorrEvOd,...
         'VariableNames',{'mouseLine','taskType','Path','taskProb','meanFR_base','meanFR_task','meanFR_pre','meanFR_stm','meanFR_post','burstIdx','spkwv','spkwth','hfwth','ina_lastSpk_tag','ina_firstSpk_tag','ina_lastSpk_modu','ina_firstSpk_modu',...
         'pLR_tag','pLR_modu','pSaltTag','pSaltModu','statDir_tag','statDir_modu','testLatencyModu_first','testLatencyModu_second',...
         'testLatencyTag_first','testLatencyTag_second','baseLatencyModu','baseLatencyTag','pLatencyModu_first','pLatencyModu_second','pLatencyTag_first','pLatencyTag_second',...
         'lightSpk','lightPreSpk','lightPostSpk','psdPreSpk','psdPostSpk','lighttagSpk','lighttagPreSpk','lighttagPostSpk','intraLightDir','interLightDir','tagLightDir',...
-        'r_Corrbfxaft','r_Corrbfxdr','r_Corrdrxaft','r_Corrhfxhf','r_CorrEvOd','p_Corrbfxaft','p_Corrbfxdr','p_Corrdrxaft','p_Corrhfxhf','p_CorrEvOd'});
+        'peakMap','r_Corrbfxaft','r_Corrbfxdr','r_Corrdrxaft','r_Corrhfxhf','r_CorrEvOd','p_Corrbfxaft','p_Corrbfxdr','p_Corrdrxaft','p_Corrhfxhf','p_CorrEvOd'});
     
     T = [T; temT];
 end

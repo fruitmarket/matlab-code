a482eed409e51b5d619f0ccd7c15bc2463ecae49
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-10-14 19:24:07 +0900

    change

--- function/loading/cscLoad.m
+++ function/loading/cscLoad.m
@@ -2,27 +2,27 @@ function [cscTime, cscSample, cscList] = cscLoad(cscFile)
 % cscLoad searches csc files and load time sample, and list of csc data
 % [cscTime, cscSample, cscList] = cscLoad(cscFile)
 %
-%   cscTime: matrix array of time (unit: msec)
-%   cscSample: matrix array of sample (unit: mVolt)
+%   cscTime: 1D matrix array of time (unit: msec)
+%   cscSample: matrix array of samples (unit: mVolt)
 %   cscList: shows list of csc.ncs files
 %
 % Author: Junyeop Lee (cited Dohoung Kim's code)
 % Version 1.0 (Oct, 13, 2016)
 
 if nargin == 0;
-    cscList = FindFiles('CSC*.ncs','CheckSubdirs',1);
+    cscList = FindFiles('CSC*.ncs','CheckSubdirs',0);
 else
     if ~iscell(cscFile)
         disp('Input argument is wrong. It should be cell array.');
         return;
     elseif isempty(cscFile)
-        cscList = FindFiles('CSC*.ncs','CheckSubdirs',1);
+        cscList = FindFiles('CSC*.ncs','CheckSubdirs',0);
     else
         nFolder = length(cscFile);
         cscList = cell(0,1);
         for iFolder = 1:nFolder
             if exist(cscFile{iFolder},'dir')
-                cscList = [cscList; FindFiles('CSC*.ncs','StartingDirectory',fileparts(cscFile{iFolder}),'CheckSubdirs',1)];
+                cscList = [cscList; FindFiles('CSC*.ncs','StartingDirectory',fileparts(cscFile{iFolder}),'CheckSubdirs',0)];
             elseif exist(cscFile{iFolder},'file') == 2
                 [filePath, fileName, ext] = fileparts(cscFile{iFolder});
                 if strcmp(ext,'.csc')
@@ -45,7 +45,6 @@ cscSample = cell(nCSC,1);
 
 for iCSC = 1:nCSC
     [timestamp, sample] = csc2mat(cscList{iCSC});
-    cscTime{iCSC} = timestamp/1000; % cscTime unit: msec
+    cscTime = timestamp/1000; % cscTime unit: msec
     cscSample{iCSC} = sample;
-end
 end

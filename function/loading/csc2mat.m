3b24ea310fd52975452c05b9fd4b1b1a607d4940
Joonyeup Lee <jun.fruitmarket@gmail.com>
2016-10-15 21:14:45 +0900

    change

--- function/loading/csc2mat.m
+++ function/loading/csc2mat.m
@@ -8,6 +8,7 @@ function [timestamp, sample] = csc2mat(fileName)
 %   Version 1.0 (Oct/12/2016)
 
 % Import csc data
+voltageConvFactor = 10^3; % 1 means output in volts, 1000 in mV, 10^6 in uV
 [timestamps_ori, ~, ~, ~, sample, header] = Nlx2MatCSC(fileName, [1,1,1,1,1],1,1,[]);
 % [timeStamps, channelNumbers, sampleFreq, numberofValidSamples, samples, header] = Nlx2MatCSC(fileName, [1,1,1,1,1],1,1,[]);
 % numberofValidSamples: 512
@@ -18,7 +19,7 @@ voltIdx = regexp(header,'-ADBitVolts');
 voltTemp = strsplit(header{(~cellfun(@isempty,voltIdx))},' ');
 bitVolt = str2double(voltTemp{2});
 
-sample = sample(:)*bitVolt*1000; % unit: mVolt
+sample = sample(:)*bitVolt*voltageConvFactor; % unit: uVolt
 
 % timestamps rearrange
 dT = diff(timestamps_ori);
--- project-Track/spectroTrack.m
+++ project-Track/spectroTrack.m
@@ -1,9 +1,12 @@
 function spectroTrack()
-sensorWin = [1000 1000]*2; % unit: msec
 
-movingWin = [0.05 0.01]; % unit: sec
+tightInterval = [0.02 0.02]; wideInterval = [0.07 0.07];
+
+sensorWin = [-1.5 1.5]; % the number in the bracket should be in sec unit
+sensorInput = sensorWin*2*10^3;
+movingWin = [0.5 0.05]; % unit: sec
 params.Fs = 2000; % unit: Hz
-params.fpass = [0, 100];
+params.fpass = [0, 150];
 params.pad = 0;
 params.tapers = [3, 5];
 params.trialave = 1; % 0: no average, 1: average trials
@@ -19,20 +22,31 @@ channelSample = sample{1};
 idxS6 = zeros(nTrial,1);
 idxS10 = zeros(nTrial,1);
 
-sampleS6 = zeros((sum(sensorWin)+1),nTrial);
-sampleS10 = zeros((sum(sensorWin)+1),nTrial);
+sampleS6 = zeros((sum(abs(sensorInput))+1),nTrial);
+sampleS10 = zeros((sum(abs(sensorInput))+1),nTrial);
 
 
 for iTrial = 1:nTrial
     idxS6(iTrial,1) = find(sensor.S6(iTrial)<timestamp,1,'first');
-    sampleS6(:,iTrial) = channelSample((idxS6(iTrial,1)-sensorWin(1)):(idxS6(iTrial,1)+sensorWin(2)));
+    sampleS6(:,iTrial) = channelSample((idxS6(iTrial,1)+sensorInput(1)):(idxS6(iTrial,1)+sensorInput(2)));
     idxS10(iTrial,1) = find(sensor.S10(iTrial)<timestamp,1,'first');
-    sampleS10(:,iTrial) = channelSample((idxS10(iTrial,1)-sensorWin(1)):(idxS10(iTrial,1)+sensorWin(2)));
+    sampleS10(:,iTrial) = channelSample((idxS10(iTrial,1)+sensorInput(1)):(idxS10(iTrial,1)+sensorInput(2)));
 end
 
-[spectrum, time, frequencies, sError] = mtspecgramc(sampleS6/1000,movingWin,params);
-
-plot_matrix(spectrum,time,frequencies)
+[spectrumS6, timeS6, frequenciesS6, sErrorS6] = mtspecgramc(sampleS6,movingWin,params);
+[spectrumS10, timeS10, frequenciesS10, sErrorS10] = mtspecgramc(sampleS10,movingWin,params);
+hSpectro = axes('Position',axpt(2,2,1,1,[],wideInterval));
+hold on;
+hSpecField = pcolor(timeS6,frequenciesS6, spectrumS6');
+xLim = [mean(abs(sensorWin))-1, mean(abs(sensorWin))+1];
+yLim = [0, 120];
+line(xLim,[yLim(1),yLim(1)],'Color','k');
+hLine = line([xLim(1),xLim(1)],yLim,'Color','k');
+set(hSpecField,'EdgeColor','none','lineStyle','default');
+set(hSpectro,'Box','off','XLim',xLim,'TickDir','out','XTick',[xLim(1),mean(xLim),xLim(2)],'XTickLabel',{-1;0;1},'YLim',yLim);
+ylabel('Frequency (Hz)');
+xlabel('Time (sec)');
+print(gcf,'-dtiff','-r300','exercise.tiff');
 % sampleTrackLight
 % samplePlfmLight
 

ecbc54887b6fba0196be4e8656cbaa8e9266b6b0
Jun <jun.fruitmarket@gmail.com>
2016-10-17 01:16:26 +0900

    change

--- project-Track/spectroTrack.m
+++ project-Track/spectroTrack.m
@@ -1,4 +1,5 @@
 function spectroTrack()
+clc; clearvars; close all;
 
 tightInterval = [0.02 0.02]; wideInterval = [0.07 0.07];
 
@@ -9,37 +10,45 @@ params.Fs = 2000; % unit: Hz
 params.fpass = [0, 150];
 params.pad = 0;
 params.tapers = [3, 5];
-params.trialave = 1; % 0: no average, 1: average trials
+params.trialave = 0; % 0: no average, 1: average trials
 p = 0.05;
 params.err = [1, p];
 
 load('Events.mat');
-
 [timestamp, sample, cscList] = cscLoad;
+nFile = length(cscList);
 
-channelSample = sample{1};
+for iFile = 1:nFile
+    [filePath, fileName,~] = fileparts(cscList{iFile});
+    channelSample = sample{iFile};
 
-idxS6 = zeros(nTrial,1);
-idxS10 = zeros(nTrial,1);
+    if ~isempty(strfind(filePath,'DRun')|strfind(filePath,'noRun'))
+        iSensor = 6;
+    else
+        iSensor = 10;
+    end
 
-sampleS6 = zeros((sum(abs(sensorInput))+1),nTrial);
-sampleS10 = zeros((sum(abs(sensorInput))+1),nTrial);
+    idxSensor = zeros(nTrial,1);
+    sampleSensor = zeros((sum(abs(sensorInput))+1),nTrial);
+    
+    for iTrial = 1:nTrial
+        idxSensor(iTrial,1) = find(sensor.(fields{iSensor})(iTrial)<timestamp,1,'first');
+        sampleSensor(:,iTrial) = channelSample((idxSensor(iTrial,1)+sensorInput(1)):(idxSensor(iTrial,1)+sensorInput(2)));
+    end
 
-
-for iTrial = 1:nTrial
-    idxS6(iTrial,1) = find(sensor.S6(iTrial)<timestamp,1,'first');
-    sampleS6(:,iTrial) = channelSample((idxS6(iTrial,1)+sensorInput(1)):(idxS6(iTrial,1)+sensorInput(2)));
-    idxS10(iTrial,1) = find(sensor.S10(iTrial)<timestamp,1,'first');
-    sampleS10(:,iTrial) = channelSample((idxS10(iTrial,1)+sensorInput(1)):(idxS10(iTrial,1)+sensorInput(2)));
+    [specSensor, timeSensor, freqSensor, ~] = mtspecgramc(sampleSensor,movingWin,params);
+    specSensor_pre = specSensor(:,:,1:30);
+    specSensor_stm = specSensor(:,:,31:60);
+    specSensor_post = specSensor(:,:,61:90);
+    
+    save([fileName,'.mat'],'specSensor_pre','specSensor_stm','specSensor_post','timeSensor','freqSensor');
 end
 
-[spectrumS6, timeS6, frequenciesS6, sErrorS6] = mtspecgramc(sampleS6,movingWin,params);
-[spectrumS10, timeS10, frequenciesS10, sErrorS10] = mtspecgramc(sampleS10,movingWin,params);
 
 orient portrait
 hSpectro = axes('Position',axpt(6,9,1,4:5,[],wideInterval));
 hold on;
-hSpecField = pcolor(timeS6,frequenciesS6, spectrumS6');
+hSpecField = pcolor(timeSensor,freqSensor, specSensor');
 xLim = [mean(abs(sensorWin))-1, mean(abs(sensorWin))+1];
 yLim = [0, 90];
 line(xLim,[yLim(1),yLim(1)],'Color','k');

1561be8f0d290bac263aefc92189777b7f8c9a3c
Jun <jun.fruitmarket@gmail.com>
2017-01-17 15:09:24 +0900

    change

--- project-Track/plot_laserIntensity.m
+++ project-Track/plot_laserIntensity.m
@@ -29,28 +29,78 @@ paperSizeX = [18.3, 8.00];
 %%
 load('cellList_ori.mat');
 
+criteria_FR = 7;
+
 DRunPn = T.taskProb == '100' & T.taskType == 'DRun' & T.peakFR_track>1 & T.meanFR_task<criteria_FR;
 DRunIn = T.taskProb == '100' & T.taskType == 'DRun' & T.peakFR_track>1 & T.meanFR_task>criteria_FR;
 DRwPn = T.taskProb == '100' & T.taskType == 'DRw' & T.peakFR_track>1 & T.meanFR_task<criteria_FR;
 DRwIn = T.taskProb == '100' & T.taskType == 'DRw' & T.peakFR_track>1 & T.meanFR_task>criteria_FR;
 
 plfmLight = T.statDir_Plfm2hz ~= 0 & T.pLR_Plfm2hz<0.005;
-trackLight = T.statDir_Track ~= 0 & T.pLR_Track<0.005;
 
 % none/either/both : Activated on Platform or Track?
-DRunPn_none = DRunPn & ~(plfmLight | trackLight);
-DRunPn_either = DRunPn & (plfmLight | trackLight);
-DRunPn_both = DRunPn & (plfmLight & trackLight);
+DRunPn_evoked = DRunPn & plfmLight;
+DRunPn_notevoked = DRunPn & ~plfmLight;
 
-DRunIn_none = DRunIn & ~(plfmLight | trackLight);
-DRunIn_either = DRunIn & (plfmLight | trackLight);
-DRunIn_both = DRunIn & (plfmLight & trackLight);
+DRunIn_evoked = DRunIn & plfmLight;
+DRunIn_noevoked = DRunIn & ~plfmLight;
 
-DRwPn_none = DRwPn & ~(plfmLight | trackLight);
-DRwPn_either = DRwPn & (plfmLight | trackLight);
-DRwPn_both = DRwPn & (plfmLight & trackLight);
+DRwPn_evoked = DRwPn & plfmLight;
+DRwPn_notevoked = DRwPn & ~plfmLight;
 
-DRwIn_none = DRwIn & ~(plfmLight | trackLight);
-DRwIn_either = DRwIn & (plfmLight | trackLight);
-DRwIn_both = DRwIn & (plfmLight & trackLight);
+DRwIn_evoked = DRwIn & plfmLight;
+DRwIn_notevoked = DRwIn & ~plfmLight;
 
+lightPlfm5mw_DRunPn = T.lightPlfmSpk5mw(DRunPn_evoked);
+lightPlfm8mw_DRunPn = T.lightPlfmSpk8mw(DRunPn_evoked);
+lightPlfm10mw_DRunPn = T.lightPlfmSpk10mw(DRunPn_evoked);
+
+lightPlfm5mw_DRunIn = T.lightPlfmSpk5mw(DRunIn_evoked);
+lightPlfm8mw_DRunIn = T.lightPlfmSpk8mw(DRunIn_evoked);
+lightPlfm10mw_DRunIn = T.lightPlfmSpk10mw(DRunIn_evoked);
+
+lightPlfm5mw_DRwPn = T.lightPlfmSpk5mw(DRwPn_evoked);
+lightPlfm8mw_DRwPn = T.lightPlfmSpk8mw(DRwPn_evoked);
+lightPlfm10mw_DRwPn = T.lightPlfmSpk10mw(DRwPn_evoked);
+
+lightPlfm5mw_DRwIn = T.lightPlfmSpk5mw(DRwIn_evoked);
+lightPlfm8mw_DRwIn = T.lightPlfmSpk8mw(DRwIn_evoked);
+lightPlfm10mw_DRwIn = T.lightPlfmSpk10mw(DRwIn_evoked);
+
+
+%% Figure (DRun)
+hInten(1) = axes('Position',axpt(2,2,1,1,[0.1 0.1 0.85 0.85],wideInterval));
+plot([1,2,3],[lightPlfm5mw_DRunPn, lightPlfm8mw_DRunPn, lightPlfm10mw_DRunPn],'-o','Color',colorGray,'MarkerFaceColor',colorBlue,'MarkerEdgeColor','k','MarkerSize',markerM);
+hold on;
+plot([1,2,3],[mean(lightPlfm5mw_DRunPn), mean(lightPlfm8mw_DRunPn), mean(lightPlfm10mw_DRunPn)],'-o','Color','k','MarkerFaceColor','k','LineWidth',2,'MarkerSize',markerM);
+text(3.5, 50,['n = ',num2str(length(lightPlfm5mw_DRunPn))]);
+ylabel('Spike counts');
+
+hInten(2) = axes('Position',axpt(2,2,1,2,[0.1 0.1 0.85 0.85], wideInterval));
+plot([1,2,3],[lightPlfm5mw_DRunIn, lightPlfm8mw_DRunIn, lightPlfm10mw_DRunIn],'-o','Color',colorGray,'MarkerFaceColor',colorBlue,'MarkerEdgeColor','k','MarkerSize',markerM);
+hold on;
+plot([1,2,3],[mean(lightPlfm5mw_DRunIn), mean(lightPlfm8mw_DRunIn), mean(lightPlfm10mw_DRunIn)],'-o','Color','k','MarkerFaceColor','k','LineWidth',2,'MarkerSize',markerM);
+text(3.5, 50,['n = ',num2str(length(lightPlfm5mw_DRunIn))]);
+ylabel('Spike counts');
+
+hInten(3) = axes('Position',axpt(2,2,2,1,[0.1 0.1 0.85 0.85], wideInterval));
+plot([1,2,3],[lightPlfm5mw_DRwPn, lightPlfm8mw_DRwPn, lightPlfm10mw_DRwPn],'-o','Color',colorGray,'MarkerFaceColor',colorBlue,'MarkerEdgeColor','k','MarkerSize',markerM);
+hold on;
+plot([1,2,3],[mean(lightPlfm5mw_DRwPn), mean(lightPlfm8mw_DRwPn), mean(lightPlfm10mw_DRwPn)],'-o','Color','k','MarkerFaceColor','k','LineWidth',2,'MarkerSize',markerM);
+text(3.5, 50,['n = ',num2str(length(lightPlfm5mw_DRwPn))]);
+ylabel('Spike counts');
+
+hInten(4) = axes('Position',axpt(2,2,2,2,[0.1 0.1 0.85 0.85], wideInterval));
+plot([1,2,3],[lightPlfm5mw_DRwIn, lightPlfm8mw_DRwIn, lightPlfm10mw_DRwIn],'-o','Color',colorGray,'MarkerFaceColor',colorBlue,'MarkerEdgeColor','k','MarkerSize',markerM);
+hold on;
+plot([1,2,3],[mean(lightPlfm5mw_DRwIn), mean(lightPlfm8mw_DRwIn), mean(lightPlfm10mw_DRwIn)],'-o','Color','k','MarkerFaceColor','k','LineWidth',2,'MarkerSize',markerM);
+text(3.5, 50,['n = ',num2str(length(lightPlfm5mw_DRwIn))]);
+ylabel('Spike counts');
+
+set(hInten,'Box','off','TickDir','out','XLim',[0,4],'XTick',[1:3],'XTickLabel',{'5mW','8mW','10mW'});
+set(hInten(1),'YLim',[0,300]);
+set(hInten(2),'YLim',[0,400]);
+set(hInten(3),'YLim',[0,300]);
+set(hInten(4),'YLim',[0,400]);
+
+print('-painters','laserIntensity2hz','-r300','-dtiff');

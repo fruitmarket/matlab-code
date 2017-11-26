ed4620e3ae3044d8594e12e247267b3d64581d99
Joonyeup Lee <jun.fruitmarket@gmail.com>
2017-11-26 13:39:31 +0900

    change

--- project-Track/analysis_laserSpikeChange50hz.m
+++ project-Track/analysis_laserSpikeChange50hz.m
@@ -9,6 +9,7 @@ function analysis_laserSpikeChange50hz()
 resolution = 10; % sigma = resoution * binSize = 100 msec
 winTrack = [0 125];
 binSizeBlue = 2;
+winCriProb = [0, 20];
 
 [tData, tList] = tLoad;
 nCell = length(tList);
@@ -20,25 +21,43 @@ for iCell = 1:nCell
 
     % Load Events variables
     load('Events.mat');
+%% On Track    
+    if ~isempty(lightTime.Track50hz)
+        spkTimeTrack50hz = spikeWin(tData{iCell},lightTime.Track50hz,winCriProb);
+        lightProbTrack_50hz = sum(double(~cellfun(@isempty,spkTimeTrack50hz)))/length(lightTime.Track50hz)*100;
+    else
+        lightProbTrack_50hz = NaN;            
+    end
+    
+    if isfield(lightTime,'Track2hz')
+        if ~isempty(lightTime.Track2hz)
+            spkTimeTrack2hz = spikeWin(tData{iCell},lightTime.Track2hz,winCriProb);
+            lightProbTrack_2hz = sum(double(~cellfun(@isempty,spkTimeTrack2hz)))/length(lightTime.Track2hz)*100;
+        else
+            lightProbTrack_2hz = NaN;
+        end
+    end
+    save([cellName,'.mat'],'lightProbTrack_2hz','lightProbTrack_50hz','-append');
+%% On Platform
+    if isfield(lightTime,'Plfm2hz')
+        if ~isempty(lightTime.Plfm2hz)
+            spkTime2hz = spikeWin(tData{iCell},lightTime.Plfm2hz(201:400),winCriProb);
+            lightProbPlfm_2hz = sum(double(~cellfun(@isempty,spkTime2hz)))/length(lightTime.Plfm2hz(201:400))*100;        
+        else
+            lightProbPlfm_2hz = NaN;
+        end
+    end
+    
+    if isfield(lightTime,'Plfm50hz')
+        if ~isempty(lightTime.Plfm50hz)
+            spkTime50hz = spikeWin(tData{iCell},lightTime.Plfm50hz,winCriProb);
+            lightProbPlfm_50hz = sum(double(~cellfun(@isempty,spkTime50hz)))/length(lightTime.Plfm50hz)*100;
+        else
+            lightProbPlfm_50hz = NaN;            
+        end
+    end
+    save([cellName,'.mat'],'lightProbPlfm_2hz','lightProbPlfm_50hz','-append');
 
-% Light - ontrack
-    evoSpikeTimeTrack = spikeWin(tData{iCell},lightTime.Track8hz,winTrack);
-    [evoXptTrackLight, ~, ~, ~, ~, ~] = rasterPETH(evoSpikeTimeTrack,true(size(lightTime.Track8hz)),winTrack,binSizeBlue,resolution,1);     
-    evoSpike_stmEarly = sum(0<evoXptTrackLight{1} & evoXptTrackLight{1}<60);
-    evoSpike_stmLate = sum(65<evoXptTrackLight{1} & evoXptTrackLight{1}<125);
-
-% Pseudo light (On track)
-    evoSpikeTime_psdPre = spikeWin(tData{iCell},psdlightPre,winTrack); % Pseudo light Pre
-    [evoXptPsdPre, ~, ~, ~, ~, ~] = rasterPETH(evoSpikeTime_psdPre,true(size(psdlightPre)),winTrack,binSizeBlue,resolution,1);
-    evoSpike_preEarly = sum(0<evoXptPsdPre{1} & evoXptPsdPre{1}<60);
-    evoSpike_preLate = sum(65<evoXptPsdPre{1} & evoXptPsdPre{1}<125);
-
-    evoSpikeTime_psdPost = spikeWin(tData{iCell},psdlightPost,winTrack); % Pseudo light Post
-    [evoXptPsdPost, ~, ~, ~, ~, ~] = rasterPETH(evoSpikeTime_psdPost,true(size(psdlightPost)),winTrack,binSizeBlue,resolution,1);
-    evoSpike_postEarly = sum(0<evoXptPsdPost{1} & evoXptPsdPost{1}<60);
-    evoSpike_postLate = sum(65<evoXptPsdPost{1} & evoXptPsdPost{1}<125);
-
-    save([cellName,'.mat'],'evoXptTrackLight','evoXptPsdPre','evoXptPsdPost','evoSpike_stmEarly','evoSpike_stmLate','evoSpike_preEarly','evoSpike_preLate','evoSpike_postEarly','evoSpike_postLate','-append');
 end
 disp('### Calculating Light induced spike change is done! ###');
 
